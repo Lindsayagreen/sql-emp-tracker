8f23275404a566964d6860d6b278d8c14d9f7b0c
lindsayagreen <lindsgree@gmail.com>
2023-11-28 09:52:52 -0600

    setup boiler for seed and schema

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- database-driven application, typically contains SQL statements to populate the databases withinital oor sample data. Seeding refers to the process of adding data to a database, and seeds diles are often used during the development or teating phases of an application to ensure tge database has some predefined data for testing or demonstration purposes
+-- populates data into the tables defined in the schema.sql
+-- this file uses the INSERT command 
+
+INSERT INTO 
+
+VALUES
