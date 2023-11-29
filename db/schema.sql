0fca4d364d2bb757bc9ce8ea4dba8c37468d057e
lindsayagreen <lindsgree@gmail.com>
2023-11-29 12:16:53 -0600

    added boiler to schema and psuedo code

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,13 @@
 -- file typically used to define the structure and organization of a database.
 -- contains SQL statements that create tables, defines relationships between tables, data types, and other info involving data.
 
-DROP DATABASE IF EXISTS
--- DELETES DATABASE 
--- FIRST STEP IN THE SETUP
+DROP DATABASE IF EXISTS emplist_db;
 
-CREATE DATABASE
--- CREATES DATABASE
--- SECOND STEP IN THE SETUP
+CREATE DATABASE emplist_db;
 
-USE
+USE emplist_db;
 -- command used to specify which database should be the current or default database for the current database session.
+
 
 CREATE TABLE department (
 
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 // Importing the necessary modules: Express for creating a web server and mysql2 for interacting with MySQL databases.
 const express = require('express');
 const mysql = require('mysql2');
@@ -38,3 +40,37 @@ app.use((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const initQuestions = () => inquirer.prompt({
+  type: "list",
+  choices: ["Add department", "Add role", "Add employee", "View departments", "View roles", "View employees", "Update employee role", "Quit"],
+  message: "What would you like to do?",
+  name: "option"
+})
+.then(result.option) {
+
+}
+  
+//department function
+//resulting in table showing dept names and dept ids
+
+// view all roles function
+//presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+//view all employees function
+//resulting in table employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+//choose to ADD dept
+//prompted to enter the name of the department and that department is added to the database
+inquirer.prompt
+//choose ADD role
+//prompted to enter the name, salary, and department for the role and that role is added to the database
+inquirer.prompt
+
+//choose to ADD employee
+//prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+inquirer.prompt
+
+//choose to update an employee role
+// prompted to select an employee to update and their new role and this information is updated in the database
+inquirer.prompt
